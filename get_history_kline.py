2d9bf57b90f58a66b3f9e15b2c7ce6344a179b47
张洋荣 <670032682@qq.com>
2024-10-16 18:26:52 +0800

    v2

--- get_history_kline.py
+++ get_history_kline.py
@@ -3,22 +3,28 @@ import time
 from datetime import datetime
 from concurrent import futures
 import argparse
+import pandas as pd
 from utils.get_bybit_data import *
 parser = argparse.ArgumentParser(description='test')
 parser.add_argument('--num_workers',type=int,default=30)
 args = parser.parse_args()
 
+start_date = 20230101
+
+def date_to_unix(date_str):
+    dt = datetime.strptime(str(date_str), "%Y%m%d")
+    return int(dt.timestamp())
+
 if __name__ == "__main__":
-    while True:
-        current_seconds = datetime.now().second
-        if current_seconds <= 4:
-            time.sleep(1)
-        symbols = get_symbols()
+    start_timestamp = date_to_unix(start_date)
+    end_timestamp = int(time.time())
+    symbols = get_symbols()
+    
+    for t in range(start_timestamp,end_timestamp,1000):
         the_futures = []
-        now_time = int(time.time())
         with futures.ThreadPoolExecutor(max_workers=args.num_workers) as executor:
             for symbol in symbols:
-                future = executor.submit(fetch_symbol_data_parallel,symbol,now_time)
+                future = executor.submit(fetch_symbol_data_parallel_his,symbol,t)
                 the_futures.append(future)
         for future in futures.as_completed(the_futures):
             pass
--- utils/get_bybit_data.py
+++ utils/get_bybit_data.py
@@ -21,8 +21,30 @@ def get_minute_ohlcv(symbol,now_time):
         return response["result"]['list'][-1]  # 返回最新的K线数据
     return None
 
+def get_1000_ohlcv(symbol,now_time):
+    url = "https://api.bybit.com/v5/market/kline"
+    params = {
+        "symbol": symbol,
+        "interval": "1",  # 1分钟K线
+        "start": (now_time)*1000,  # 获取最近1分钟的K线
+        "end": (now_time+60*999)*1000,  # 获取最近1分钟的K线
+        "limit": 1000,
+    }
+    response = requests.get(url, params=params).json()
+    if response["retCode"] == 0 and response["result"]:
+        result = response["result"]
+        return response["result"]['list']  # 返回最新的K线数据
+    return None
+
 def fetch_symbol_data_parallel(symbol,now_time):
     data = get_minute_ohlcv(symbol,now_time)
+    if data:
+        print(f"Symbol: {symbol}, Kline: {data}")
+    else:
+        print(f"No data for {symbol}")
+
+def fetch_symbol_data_parallel_his(symbol,now_time):
+    data = get_1000_ohlcv(symbol,now_time)
     if data:
         print(f"Symbol: {symbol}, Kline: {data}")
     else:
